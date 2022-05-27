f0dd69c6e222cafa667fe258640aa56f5ef0e258
WestXL <L.Xiao.191@westcliff.edu>
2022-05-27 09:32:16 -0700

    w4 Lab

---
+++ js/gallery_cat.js
@@ -0,0 +1,40 @@
+var photos = [];
+var fn = [];
+var imageList = [];
+var image;
+var head = "<li class='photo'>";
+var tail = "</li>";
+var disArray = ["His name is Mbappe!",
+    "Her name is Joe Cole!",
+    "Her name is Gerrard!",
+    "His name is Lampard!",
+    "His name is Ashley Cole!",
+    "His name is Ferdinand!",
+    "His name is Terry!",
+    "Her name is Gary Neville!",
+    "Her name is Haaland!",
+    "Her name is Pelle!"
+];
+
+for (var i = 1; i <= 10; i++) {
+    captionText = "This is Kitten No. " + i;
+    image = head + "<img src='/images/cats/cat-" +
+        i + ".jpg'>" +
+        "<div class='hiddenText' onclick='on(" + i + ")'>Are you interested in Kitten-" +
+        i + "?</div>" + "<figcaption class='caption'>" +
+        captionText + "</figcaption>" + tail;
+    imageList.push(image);
+}
+
+document.getElementById("photos").innerHTML = imageList;
+
+function on(i) {
+    document.getElementById("overlay").style.display = "block";
+    var str = "<p>" + disArray[i - 1] + "</p>";
+    document.getElementById("text").innerHTML = "<h4>You have selected Kitten " + i + "!</h4>" +
+        str + "<div id='closeBtn' onclick='off()'>click here to close the discription<div>";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
---
+++ js/gallery_dog.js
@@ -0,0 +1,40 @@
+var photos = [];
+var fn = [];
+var imageList = [];
+var image;
+var head = "<li class='photo'>";
+var tail = "</li>";
+var disArray = ["His name is Owen!",
+    "Her name is Zidane!",
+    "Her name is Ronaldo!",
+    "His name is Zico!",
+    "His name is Messi!",
+    "His name is Neymar!",
+    "His name is Beckham!",
+    "Her name is Rooney!",
+    "Her name is Cantona!",
+    "Her name is Totti!"
+];
+
+for (var i = 1; i <= 10; i++) {
+    captionText = "This is Puppy No. " + i;
+    image = head + "<img src='/images/dogs/dog-" +
+        i + ".jpg'>" +
+        "<div class='hiddenText' onclick='on(" + i + ")'>Are you interested in Puppy-" +
+        i + "?</div>" + "<figcaption class='caption'>" +
+        captionText + "</figcaption>" + tail;
+    imageList.push(image);
+}
+
+document.getElementById("photos").innerHTML = imageList;
+
+function on(i) {
+    document.getElementById("overlay").style.display = "block";
+    var str = "<p>" + disArray[i - 1] + "</p>";
+    document.getElementById("text").innerHTML = "<h4>You have selected Puppy " + i + "!</h4>" +
+        str + "<div id='closeBtn' onclick='off()'>click here to close the discription<div>";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
